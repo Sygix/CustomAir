9ed378f92d8d7d04aa53becfb48c7a6ce13ba50d
Sygix <simon18minecraft@gmail.com>
2018-05-28 14:42:24 +0200

    add CSS and start footer

--- footer.php
+++ footer.php
@@ -1,6 +1,14 @@
-        <div>
-            FOOTER
-        </div>
 
+        <footer>
+            <div> <!-- Google Map -->
+                <p>Our Location : custom air at London City airport Hartmann Rd, London E16 2PX, United Kingdom</p>
+            </div>
+            <div> <!-- Call Number -->
+
+            </div>
+            <div> <!-- Logo -->
+
+            </div>
+        </footer>
     </body>
 </html>
