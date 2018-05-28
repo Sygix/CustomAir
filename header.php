f2d34af9485ffd405af209e133f67155d85da0f4
abdelkadou <39699307+abdelkadou@users.noreply.github.com>
2018-05-28 14:39:34 +0200

    ys

--- header.php
+++ header.php
@@ -2,18 +2,21 @@
 <html>
     <head>
         <!-- En-tête de la page -->
+		<link rel="steelsheet" href="css/main.css" />
+		<link rel="steelsheet" href="css/footer.css" />
         <meta charset="utf-8" />
         <title>Custom Air</title>
     </head>
     <body>
         <header>
+			<img src="" alt="" >
             <nav>
                 <ul>
-                    <li><a href="#" alt="#" >Our feet</a><li>
-                    <li><a href="#" alt="#" >Book a flight</a><li>
-                    <li><a href="#" alt="#" >Learn to fly</a><li>
-                    <li><a href="#" alt="#" >Destination</a><li>
-                    <li><a href="#" alt="#" >Contact US</a><li>
+                    <li><a href="#" >Our feet</a><li>
+                    <li><a href="#" >Book a flight</a><li>
+                    <li><a href="#" >Learn to fly</a><li>
+                    <li><a href="#" >Destination</a><li>
+                    <li><a href="#" >Contact US</a><li>
                  <ul>
             </nav>
         </header>
