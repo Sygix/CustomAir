1eb3a2679e3d9f1ae14a35704a1749f0227935da
abdelkadou <39699307+abdelkadou@users.noreply.github.com>
2018-05-29 11:17:06 +0200

    Add files via upload

---
+++ css/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- En-tête de la page -->
+		<link rel="stylesheet" href="css/main.css" />
+		<link rel="stylesheet" href="css/footer.css" />
+        <meta charset="utf-8" />
+        <title>Custom Air</title>
+    </head>
+    <body>
+        <header>
+			<!--Logo CustomAIR-->
+		<div class="logo_li"><img src="images/logo.png" alt="Logo CustomAir" class="logo" ></div>	
+			<!--Menu PRINCIPAL -->
+		<nav >
+		
+			<ul class="menu">
+				<li><a href="index.php" class="menu_a" >Our feet</a></li>
+				<li><a href="#" class="menu_a" >Book a flight</a></li>
+				<li><a href="#" class="menu_a" >Learn to fly</a></li>
+				<li><a href="#" class="menu_a" >Destination</a></li>
+				<li><a href="#" class="menu_a" >Contact US</a></li>
+			</ul>
+        </nav>
+		<ul class="reseaux_sociaux">
+			<li><a href="#" class="reseaux_sociaux_a" ><img src="images/facebook_icon.png" alt="Facebook" class="reseaux_sociaux_img" ></a></li>
+			<li><a href="#" class="reseaux_sociaux_a" ><img src="images/twitter_icon.png" alt="Twitter" class="reseaux_sociaux_img" ></a></li>
+			<li><a href="#" class="reseaux_sociaux_a" ><img src="images/facebook_icon.png" alt="Linkedin" class="reseaux_sociaux_img"></a></li>
+		</ul>		
+			<a href="#" >SIGN IN</a>
+			<img src="" alt="" >
+        </header>
