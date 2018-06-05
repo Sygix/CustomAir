f98219edb6f23e68a5dcdd560c16954e99a25fe1
abdelkadou <39699307+abdelkadou@users.noreply.github.com>
2018-06-05 08:45:21 +0200

    isisisisi

--- header.php
+++ header.php
@@ -4,16 +4,18 @@
         <!-- En-tête de la page -->
 		<link rel="stylesheet" href="css/main.css" />
 		<link rel="stylesheet" href="css/footer.css" />
-        <meta charset="utf-8" />
+		<link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		
+		<meta charset="utf-8" />
         <title>Custom Air</title>
     </head>
     <body>
         <header>
 			<!--Logo CustomAIR-->
-		<div class="logo_li"><img src="img/logo.png" alt="Logo CustomAir" class="logo" ></div>
+		<div class="logo_li"><img src="img/logo.png" alt="Logo CustomAir" class="logo" ></div>	
 			<!--Menu PRINCIPAL -->
-		<nav >
-		
+		<nav>
 			<ul class="menu">
 				<li><a href="index.php" class="menu_a" >Our feet</a></li>
 				<li><a href="#" class="menu_a" >Book a flight</a></li>
@@ -22,11 +24,16 @@
 				<li><a href="#" class="menu_a" >Contact US</a></li>
 			</ul>
         </nav>
+		<div class="menu2">
 		<ul class="reseaux_sociaux">
-			<li><a href="#" class="reseaux_sociaux_a" ><img src="img/facebook_icon.png" alt="Facebook" class="reseaux_sociaux_img" ></a></li>
-			<li><a href="#" class="reseaux_sociaux_a" ><img src="img/twitter_icon.png" alt="Twitter" class="reseaux_sociaux_img" ></a></li>
-			<li><a href="#" class="reseaux_sociaux_a" ><img src="img/facebook_icon.png" alt="Linkedin" class="reseaux_sociaux_img"></a></li>
-		</ul>		
-			<a href="#" >SIGN IN</a>
-			<img src="" alt="" >
+			<li class="reseaux_sociaux_li"><a href="#" class="reseaux_sociaux_a" ><img src="img/facebook_icon.png" alt="Facebook" class="reseaux_sociaux_img" ></a></li>
+			<li class="reseaux_sociaux_li"><a href="#" class="reseaux_sociaux_a" ><img src="img/twitter_icon.png" alt="Twitter" class="reseaux_sociaux_img" ></a></li>
+			<li class="reseaux_sociaux_li"><a href="#" class="reseaux_sociaux_a" ><img src="img/facebook_icon.png" alt="Linkedin" class="reseaux_sociaux_img"></a></li>
+			
+		</ul>
+		<ul class="market">
+		<li ><a href="#"  >SIGN IN</a></li>
+		<li class="market_li"><img src="img/market_icon.png" class="market_img"alt="" ></li>
+		</ul>
+		</div>
         </header>
